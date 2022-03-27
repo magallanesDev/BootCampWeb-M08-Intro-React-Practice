08a86f1e382bdc76e2933e895fb66eb6f271fca0
Mario Filgueiras <magallanesdeveloper@gmail.com>
2022-03-27 11:16:12 +0200

    All filters working

--- nodepop/src/components/adverts/AdvertsPage/AdvertsPage.js
+++ nodepop/src/components/adverts/AdvertsPage/AdvertsPage.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useCallback, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Page from '../../layout/Page';
 import Button from '../../common/Button';
@@ -19,15 +19,33 @@ const EmptyList = () => (
 
 const AdvertsPage = () => {
   const [adverts, setAdverts] = useState([]);
-  const [searchName, setSearchName] = useState('');
+  const [filters, setFilters] = useState({
+    searchName: '',
+    searchSale: '',
+    searchTags: '',
+    searchPrice: '',
+});
+  
 
-  const handleChange = event => {
-    setSearchName(event.target.value);
-    console.log(event.target.value);
-  };
+  const { searchName, searchSale, searchTags, searchPrice} = filters;
 
+  const handleChange = useCallback(event => {
+    setFilters(filters => ({
+      ...filters,
+      [event.target.name] : event.target.value,
+    }));
+  }, []);
+
+  
+  
   const filteredAdverts = adverts.filter(advert => {
-    if (advert.name.toLowerCase().includes(searchName.toLowerCase())) {
+    
+    if (
+      (advert.name.toLowerCase().includes(searchName.toLowerCase()))  &&
+      (String(advert.sale).toLowerCase().includes(searchSale.toLowerCase())) &&
+      (String(advert.tags).toLowerCase().includes(searchTags.toLowerCase())) &&
+      (String(advert.price).includes(searchPrice))
+    ) { 
       return true;
     }
     return false;
@@ -55,6 +73,30 @@ const AdvertsPage = () => {
                 label="Filter by name"
                 className="loginForm-field"
                 value={searchName}
+                onChange={handleChange}
+              />
+              <FormField
+                type="text"
+                name="searchSale"
+                label="Filter by sale"
+                className="loginForm-field"
+                value={searchSale}
+                onChange={handleChange}
+              />
+              <FormField
+                type="text"
+                name="searchTags"
+                label="Filter by tags"
+                className="loginForm-field"
+                value={searchTags}
+                onChange={handleChange}
+              />
+              <FormField
+                type="number"
+                name="searchPrice"
+                label="Filter by price"
+                className="loginForm-field"
+                value={searchPrice}
                 onChange={handleChange}
               />
             </form>
